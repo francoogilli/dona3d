814b46e79e4a97e8b89f662381d3481b0c57ecc9
Franco Gilli <francogilli10@gmail.com>
2024-02-08 11:00:52 -0300

    Add background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`bg-white dark:bg-[#050505] ${montserrat.className}`}>
+      <body className={`${montserrat.className}`}>
         <Providers>
+        <div className="absolute dark:hidden inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div> 
+        <div className="absolute hidden dark:block inset-0 -z-10 h-full w-full bg-[#050505] bg-[linear-gradient(to_right,#0a0a0a_1px,transparent_1px),linear-gradient(to_bottom,#0a0a0a_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#051724,transparent)]"></div></div>
           <div className="flex">
             <main className="flex-1">
               <Header />
