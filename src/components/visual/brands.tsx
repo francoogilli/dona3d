42f34e47102609defc4ea392bab904e5a74cd5e4
Franco Gilli <francogilli10@gmail.com>
2024-02-10 18:24:05 -0300

    Fix card hover pointer and select image

--- src/components/visual/brands.tsx
+++ src/components/visual/brands.tsx
@@ -8,22 +8,22 @@ export default function Brands(){
         <div className="mt-12 border-y border-gray-400 py-8 text-center rounded-3xl dark:border-gray-800 md:py-7 xl:mt-20">
             <div className="w-full sm:w-2/3 mx-auto grid grid-cols-2 sm:grid-cols-4  gap-y-3 justify-center items-center">
                 <div className="flex h-24 items-center justify-center">
-                    <Image src="/prusadark.svg" className="hidden dark:block" alt="brand" width={160} height={160} />
-                    <Image src="/prusalight.svg" className="block dark:hidden" alt="brand" width={160} height={160} />
+                    <Image src="/prusadark.svg" draggable={false} className="hidden select-none dark:block" alt="brand" width={160} height={160} />
+                    <Image src="/prusalight.svg" draggable={false} className="block select-none dark:hidden" alt="brand" width={160} height={160} />
                 </div>
                 
                 <div className="flex h-24 items-center justify-center">
-                    <Image src="/grilon3dark.svg" className="hidden dark:block" alt="brand" width={160} height={160} />
-                    <Image src="/grilon3light.svg" className="block dark:hidden" alt="brand" width={160} height={160} />
+                    <Image src="/grilon3dark.svg" draggable={false} className="hidden select-none dark:block" alt="brand" width={160} height={160} />
+                    <Image src="/grilon3light.svg" draggable={false} className="block select-none dark:hidden" alt="brand" width={160} height={160} />
                 </div>
                 <div className="flex h-24 items-center justify-center">
-                    <Image src="/printalotdark.svg" className="hidden dark:block" alt="brand" width={160} height={160} />
-                    <Image src="/printalotlight.svg" className="block dark:hidden" alt="brand" width={160} height={160} />
+                    <Image src="/printalotdark.svg" draggable={false} className="hidden select-none dark:block" alt="brand" width={160} height={160} />
+                    <Image src="/printalotlight.svg" draggable={false} className="block select-none dark:hidden" alt="brand" width={160} height={160} />
                 </div>
                 
                 <div className="flex h-24 items-center justify-center">
-                    <Image src="/crealitydark.svg" className="hidden dark:block" alt="brand" width={160} height={160} />
-                    <Image src="/crealitylight.svg" className="block dark:hidden" alt="brand" width={160} height={160} />
+                    <Image src="/crealitydark.svg" draggable={false} className="hidden select-none dark:block" alt="brand" width={160} height={160} />
+                    <Image src="/crealitylight.svg" draggable={false} className="block select-none dark:hidden" alt="brand" width={160} height={160} />
                 </div>
             </div>
         </div>
