9580018d31f0d59abe10cae7ec8f11e8877758e7
Franco Gilli <francogilli10@gmail.com>
2024-02-10 15:36:30 -0300

    Change brands title

--- src/components/visual/brands.tsx
+++ src/components/visual/brands.tsx
@@ -3,7 +3,7 @@ export default function Brands(){
     return(
         
         <> 
-            <h2 className="text-3xl text-center font-bold tracking-tighter pb-5 sm:text-4xl xl:text-5xl/none bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-black dark:from-white dark:to-gray-200">Marcas que nos proveen</h2>
+            <h2 className="text-3xl text-center font-bold tracking-tighter pb-5 sm:text-4xl xl:text-5xl/none bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-black dark:from-white dark:to-gray-200">Nuestros proveedores</h2>
 
         <div className="mt-12 border-y border-gray-400 py-8 text-center rounded-3xl dark:border-gray-800 md:py-7 xl:mt-20">
             <div className="w-full flex flex-wrap gap-x-14 gap-y-3 justify-center items-center">
