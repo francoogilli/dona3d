0b447be940264b622912d8b3cc64b953696c3b10
Franco Gilli <francogilli10@gmail.com>
2024-02-10 15:26:07 -0300

    Accommodate home page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import Hero from "src/components/visual/hero";
 import FeaturesGrid from "src/components/visual/features-grid";
+import Brands from "src/components/visual/brands";
+import FAQ from "src/components/visual/faq";
 export default function Home() {
   return (
     <>
             <Hero />
            <FeaturesGrid />
+           <Brands />
+           <FAQ />
     </>
   );
 }
--- src/components/visual/brands.tsx
+++ src/components/visual/brands.tsx
@@ -3,9 +3,9 @@ export default function Brands(){
     return(
         
         <> 
-            <h2 className="text-3xl text-center font-bold tracking-tighter sm:text-4xl xl:text-5xl/none bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-black dark:from-white dark:to-gray-500">Marcas que nos proveen</h2>
+            <h2 className="text-3xl text-center font-bold tracking-tighter pb-5 sm:text-4xl xl:text-5xl/none bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-black dark:from-white dark:to-gray-200">Marcas que nos proveen</h2>
 
-        <div className="mt-12 border-y border-gray-400 py-8 text-center rounded-3xl dark:border-gray-800 md:py-12 xl:mt-20">
+        <div className="mt-12 border-y border-gray-400 py-8 text-center rounded-3xl dark:border-gray-800 md:py-7 xl:mt-20">
             <div className="w-full flex flex-wrap gap-x-14 gap-y-3 justify-center items-center">
                 <div className="flex h-24 items-center justify-center">
                     <Image src="/prusadark.svg" className="hidden dark:block" alt="brand" width={160} height={160} />
