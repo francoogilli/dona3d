58f5172810785922141477cacc5ecedffb09cc09
Franco Gilli <francogilli10@gmail.com>
2024-02-11 12:38:14 -0300

    Add prototype materials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Hero from "src/components/visual/hero";
 import FeaturesGrid from "src/components/visual/features-grid";
 import Brands from "src/components/visual/brands";
 import FAQ from "src/components/visual/faq";
+import Materials from "src/components/visual/materials";
 export default function Home() {
   return (
     <>
             <Hero />
            <FeaturesGrid />
+            <Materials />
            <Brands />
            <FAQ />
     </>
