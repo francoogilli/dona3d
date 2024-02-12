afa90846960126d55b4f56b9c1974c24dc2a798e
Franco Gilli <francogilli10@gmail.com>
2024-02-12 08:13:50 -0300

    Fix sevices title font size

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,8 +2,8 @@ import { ServicesData } from "src/data";
 export default function Services() {
     return (
       <>
-        <div className="pt-20 sm:px-5 px-2 flex flex-col items-center justify-center ">
-          <h2 className="font-bold pb-1 text-[2.65rem] sm:text-5xl lg:text-6xl ">¿Qué hacemos?</h2>
+        <div className="pt-10 sm:pt-20 sm:px-5 px-2 flex flex-col items-center justify-center ">
+          <h2 className="font-bold pb-1 text-4xl sm:text-5xl lg:text-6xl ">¿Qué hacemos?</h2>
           <div className="pt-12 grid grid-cols-1 gap-6 sm:gap-6 sm:grid-cols-2">
             {ServicesData.map((service, index) => (
               <div key={index} className="bg-neutral-900/5 dark:bg-neutral-500/5 backdrop-blur-xl rounded-2xl flex flex-col justify-center items-center p-10 border border-zinc-200 dark:border-zinc-900">
