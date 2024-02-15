796134b977f379388a7de8f381136095dea8a57d
Franco Gilli <francogilli10@gmail.com>
2024-02-15 20:28:32 -0300

    Improve spacing between elements in Brands section

--- src/components/visual/brands.tsx
+++ src/components/visual/brands.tsx
@@ -5,8 +5,8 @@ export default function Brands(){
         <> 
             <h2 className="text-3xl text-center font-bold tracking-tighter pb-5 sm:text-4xl xl:text-5xl/none bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-black dark:from-white dark:to-gray-200">Nuestros proveedores</h2>
 
-        <div className="mt-1 border-y border-gray-400 py-8 text-center rounded-3xl dark:border-gray-800 md:py-7 xl:mt-2">
-            <div className="w-full sm:w-2/3 mx-auto grid grid-cols-2 sm:grid-cols-4  gap-y-3 justify-center items-center">
+        <div className="mt-1 border-y border-zinc-300 py-8 text-center rounded-3xl dark:border-zinc-900 md:py-7 xl:mt-2">
+            <div className="w-full sm:w-11/12 mx-auto grid grid-cols-2 sm:grid-cols-4  gap-y-3 justify-center items-center">
                 <div className="flex h-24 items-center justify-center">
                     <Image src="/prusadark.svg" draggable={false} className="hidden select-none dark:block" alt="brand" width={160} height={160} />
                     <Image src="/prusalight.svg" draggable={false} className="block select-none dark:hidden" alt="brand" width={160} height={160} />
@@ -32,6 +32,3 @@ export default function Brands(){
     )
 }
 
-/*
-  <h2 className="text-2xl uppercase text-center font-bold pb-2 text-gray-800 dark:text-neutral-400">Marcas que nos proveen</h2>
-*/
