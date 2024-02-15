a2530247f2b6907fc0d5e127f4f07e54f0380827
Franco Gilli <francogilli10@gmail.com>
2024-02-15 11:26:04 -0300

    Add filament roll images

--- src/components/visual/materials.tsx
+++ src/components/visual/materials.tsx
@@ -7,27 +7,27 @@ export default function Materials() {
                 <div className="flex flex-col items-center">
                     <p className="font-bold text-2xl pb-2">PLA</p>
                     <div className="sm:size-56 size-36 flex justify-center items-center border  bg-zinc-100 dark:bg-neutral-950 border-zinc-300 dark:border-zinc-800 rounded-3xl">
-                    <Image src="/Cube.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
+                    <Image src="/rollo.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
                     
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <p className="font-bold text-2xl pb-2">PET-G</p>
                     <div className="sm:size-56 size-36 flex justify-center items-center border  bg-zinc-100 dark:bg-neutral-950 border-zinc-300 dark:border-zinc-800 rounded-3xl">
-                        <Image src="/Cube.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
+                        <Image src="/rollo2.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <p className="font-bold text-2xl pb-2">FLEX</p>
                     <div className="sm:size-56 size-36 flex justify-center items-center border  bg-zinc-100 dark:bg-neutral-950 border-zinc-300 dark:border-zinc-800 rounded-3xl">
-                    <Image src="/Cube.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
+                    <Image src="/rollo3.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
                     
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <p className="font-bold text-2xl pb-2">ABS</p>
                     <div className="sm:size-56 size-36 flex justify-center items-center border  bg-zinc-100 dark:bg-neutral-950 border-zinc-300 dark:border-zinc-800 rounded-3xl">
-                    <Image src="/Cube.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
+                    <Image src="/rollo4.png" className="size-24 sm:size-44" alt="petg" width={180} height={180}/>
                     
                     </div>
                 </div>
