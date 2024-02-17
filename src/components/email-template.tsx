08609fec3a537a77e3ea8a5e0804dc47b95d5abe
Franco Gilli <francogilli10@gmail.com>
2024-02-17 14:19:51 -0300

    Change styles of the email template that is sent

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -1,4 +1,3 @@
-// email-template.tsx
 import React from "react";
 
 interface EmailTemplateProps {
@@ -14,12 +13,14 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   email,
   message,
 }) => (
-  <div>
-    <h1>Hola, {firstName} {lastName}!</h1>
-    <p>Correo electrónico: {email}</p>
-    <p>Mensaje: {message}</p>
-    <button className="bg-sky-500 px-4 py-2">
-      <a href="https://francogilli.vercel.app/">Ir a la página</a>
-    </button>
+  <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f9fafb', padding: '20px', borderRadius: '10px', boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)' }}>
+    <h1 style={{ fontSize: '28px', color: '#333', marginBottom: '20px', textAlign: 'center', fontWeight: 'bold' }}>¡Hola, Rafael!</h1>
+    <p style={{ fontSize: '18px', color: '#555', marginBottom: '20px', textAlign: 'center' }}>¡Tienes un nuevo mensaje!</p>
+    <div style={{ backgroundColor: '#ffffff', padding: '20px', borderRadius: '10px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', marginBottom: '20px' }}>
+      <h2 style={{ fontSize: '24px', color: '#333', marginBottom: '20px', fontWeight: 'bold' }}>Detalles del mensaje:</h2>
+      <p style={{ fontSize: '18px', color: '#555', marginBottom: '10px' }}><strong>Nombre:</strong> {firstName} {lastName ? lastName : ''}</p>
+      <p style={{ fontSize: '18px', color: '#555', marginBottom: '10px' }}><strong>Correo:</strong> {email}</p>
+      <p style={{ fontSize: '18px', color: '#555', marginBottom: '10px' }}><strong>Mensaje:</strong> {message}</p>
+    </div>
   </div>
 );
