22caf256a04eebedfe0dcc638b786926899ad732
Franco Gilli <francogilli10@gmail.com>
2024-02-17 17:41:47 -0300

    Update the color of the line pattern in the background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={`bg-white dark:bg-[#050505] `}>
         <Providers>
         <div className="absolute dark:hidden inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div> 
-        <div className="absolute hidden dark:block inset-0 -z-10 h-full w-full bg-[#050505] bg-[linear-gradient(to_right,#0a0a0a_1px,transparent_1px),linear-gradient(to_bottom,#0a0a0a_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#04131d,transparent)]"></div></div>
+        <div className="absolute hidden dark:block inset-0 -z-10 h-full w-full bg-[#050505] bg-[linear-gradient(to_right,#0b0b0b_1px,transparent_1px),linear-gradient(to_bottom,#0b0b0b_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#04131d,transparent)]"></div></div>
           <div className="flex">
             <main className="flex-1">
               <Header />
