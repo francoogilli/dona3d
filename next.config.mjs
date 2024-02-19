07ca31d081944cfb6f8eea047739f167ac74bcec
Franco Gilli <francogilli10@gmail.com>
2024-02-19 08:40:36 -0300

    Change from images.domains to images.remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/dona-3d.appspot.com/o/*',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
