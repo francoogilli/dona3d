51c4b60cd854ec8355ad0b0bdf150a8c1db1b23d
Franco Gilli <francogilli10@gmail.com>
2024-02-20 11:49:25 -0300

    Add gradient to active number page in pagination

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -126,7 +126,7 @@ export default function Gallery() {
   <button
     key={i}
     onClick={() => changePage(pageNumber)}
-    className={`px-4 py-1.5 text-lg font-medium bg-[zinc-100] dark:bg-[#27272A] hover:bg-[neutral-200] dark:hover:bg-[#3F3F46] rounded-xl relative ${currentPage === pageNumber ? 'bg-blue-500 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 text-white' : ''}`}
+    className={`px-4 py-1.5 text-lg font-medium bg-[zinc-100] dark:bg-[#27272A] hover:bg-[neutral-200] dark:hover:bg-[#3F3F46] rounded-xl relative ${currentPage === pageNumber ? 'bg-gradient-to-tr to-blue-400 from-blue-600 dark:to-blue-500 dark:from-blue-700 dark:hover:bg-blue-700 text-white' : ''}`}
   >
     {pageNumber}
     {currentPage === pageNumber && <div className="absolute inset-0 shadow-blue rounded-xl" aria-hidden="true"></div>}
