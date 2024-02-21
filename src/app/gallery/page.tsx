a5bcf945c35bcd382d701060100f7b162858dffd
Franco Gilli <francogilli10@gmail.com>
2024-02-21 10:19:58 -0300

    Improve accessibility in pagination in the image gallery.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -120,6 +120,7 @@ export default function Gallery() {
       <button
   onClick={() => changePage(Math.max(currentPage - 1, 1))}
   disabled={currentPage === 1}
+  aria-label="Previous Page"
   className={`px-4 py-2.5 bg-zinc-100 dark:bg-zinc-800 hover:bg-neutral-200 dark:hover:bg-zinc-700 rounded-l-2xl ${currentPage === 1 ? 'disabled:pointer-events-none' : ''}`}
 >
   <Icon icon="mingcute:left-line" className={`${currentPage === 1 ? 'dark:text-zinc-600 text-zinc-400' : ''}`} width={20} />
@@ -142,6 +143,7 @@ export default function Gallery() {
         <button
           onClick={() => changePage(Math.min(currentPage + 1, totalPages))}
           disabled={currentPage === totalPages}
+          aria-label="Next Page"
           className={`px-4 py-2.5 bg-zinc-100 dark:bg-zinc-800 hover:bg-neutral-200 dark:hover:bg-zinc-700 rounded-r-2xl ${currentPage === totalPages ? 'disabled:pointer-events-none' : ''}`}
         >
            <Icon icon="mingcute:right-line" className={`${currentPage === totalPages ? 'dark:text-zinc-600 text-zinc-400' : ''}`} width={20} />
