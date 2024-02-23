a5fa0a1e748cf11fa632093dfab13f661e218dc9
Franco Gilli <francogilli10@gmail.com>
2024-02-22 22:44:59 -0300

    Increase service container scale when hovering

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,7 +6,7 @@ export default function Services() {
           <h2 className="font-bold pb-1 text-4xl sm:text-5xl lg:text-6xl ">¿Qué hacemos?</h2>
           <div className="pt-12 grid grid-cols-1 gap-6 sm:gap-6 sm:grid-cols-2">
             {ServicesData.map((service, index) => (
-              <div key={index} className="sm:h-[500px]  after:content relative col-span-1 row-span-3 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-2xl bg-black/5 dark:bg-white/5 px-6 pb-16 sm:pb-5 pt-64 text-center text-black dark:text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-2xl after:shadow-highlight sm:col-span-2 lg:col-span-1 lg:row-span-2 lg:pt-0">
+              <div key={index} className="sm:h-[500px] hover:scale-105 transition-transform duration-300 after:content relative col-span-1 row-span-3 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-2xl bg-black/5 dark:bg-white/5 px-6 pb-16 sm:pb-5 pt-64 text-center text-black dark:text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-2xl after:shadow-highlight sm:col-span-2 lg:col-span-1 lg:row-span-2 lg:pt-0">
               <div className=" absolute inset-0 flex items-center justify-center opacity-20">
               <span className=" flex  sm:w-[550px] sm:pt-20 max-w-[120%] sm:max-w-full items-center justify-center text-zinc-800 dark:text-zinc-400">
               {service.icon}
